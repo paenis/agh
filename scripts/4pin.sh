789bf7ddc98a0ff69f63b7ea90c685fc90a8cfbf
paenis <46578974+paenis@users.noreply.github.com>
2021-12-23 17:32:27 +0000

    Add files via upload

---
+++ scripts/4pin.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+for a in {0..9}
+do
+	for b in {0..9}
+	do
+		for c in {0..9}
+		do
+			for d in {0..9}
+			do
+				~/cs/l09c10ELF "$a$b$c$d" 
+			done
+		done
+	done
+done
+echo 'done'
